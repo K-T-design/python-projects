b89ff6dcd3f4317696f8fac3e707c6f23109704e
K-T-design <kingtitanium15@gmail.com>
2025-07-17 17:13:29 +0100

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -15,22 +15,25 @@ while True:
         break
     
     
-    
-print('What operation would you like to perform? Addition, Subtraction, Multiplication, Division, Modulus')
+while True:
+        
+    print('What operation would you like to perform? Addition, Subtraction, Multiplication, Division, Modulus')
 
-op_ = input(f'>> ')
-if (op_.lower()) == 'addition':
-    print(int(_nu1) + int(_nu2))
-elif (op_.lower()) == 'subtraction':
-    print(int(_nu1) - int(_nu2))
-elif (op_.lower()) == 'multiplication':
-    print(int(_nu1) * int(_nu2))
-elif (op_.lower() == 'division'):
-    print(int(_nu1) // int(_nu2))
-elif (op_.lower() == 'modulus'):
-    print(int(_nu1) % int(_nu2))
-else:
-    print('I\'m Very Sorry This Function Has Not Been Added To My Functionality By My Developer')
+    op_ = input(f'>> ')
+    if (op_.lower()) == 'addition':
+        print(int(_nu1) + int(_nu2))
+    elif (op_.lower()) == 'subtraction':
+        print(int(_nu1) - int(_nu2))
+    elif (op_.lower()) == 'multiplication':
+        print(int(_nu1) * int(_nu2))
+    elif (op_.lower() == 'division'):
+        print(int(_nu1) // int(_nu2))
+    elif (op_.lower() == 'modulus'):
+        print(int(_nu1) % int(_nu2))
+    else:
+        print('I\'m Very Sorry This Function Has Not Been Added To My Functionality By My Developer')
+        break
+    
     
     
           
