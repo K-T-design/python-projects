f239ff69c946645dc26fef3116ae9fdc717d2b4d
K-T-design <kingtitanium15@gmail.com>
2025-07-17 11:48:49 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,36 @@
+import math
+
+print('MY SIMPLE CALCULATOR PROJECT')
+
+name = input(f'Can I get to know your name please 🤗? ')
+
+print(f'Hello there {name}! Let\'s get your values down?')
+while True:
+    try:
+        _nu1 = int(input(f'First Value: '))
+        _nu2 = int(input(f'Second Value: '))
+    except :
+        print('The Value You Entered is Not A Number') 
+    else:
+        break
+    
+    
+    
+print('What operation would you like to perform? Addition, Subtraction, Multiplication, Division, Modulus')
+
+op_ = input(f'>> ')
+if (op_.lower()) == 'addition':
+    print(int(_nu1) + int(_nu2))
+elif (op_.lower()) == 'subtraction':
+    print(int(_nu1) - int(_nu2))
+elif (op_.lower()) == 'multiplication':
+    print(int(_nu1) * int(_nu2))
+elif (op_.lower() == 'division'):
+    print(int(_nu1) // int(_nu2))
+elif (op_.lower() == 'modulus'):
+    print(int(_nu1) % int(_nu2))
+else:
+    print('I\'m Very Sorry This Function Has Not Been Added To My Functionality By My Developer')
+    
+    
+          
