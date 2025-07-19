e74d165703140f05bb7dbb17412461eba817c305
K-T-design <kingtitanium15@gmail.com>
2025-07-19 18:53:14 +0100

    Update Weight Converter.py

--- Weight Converter.py
+++ Weight Converter.py
@@ -16,12 +16,15 @@ while True:
            conv_ = int(val_) / 2.205
         except:
             print('You did not enter a valid value')
+            continue
         else:
             print(f'Your Weight in Kilograms is {conv_}')
             if conv_ >= 75:
                print('Chimmmooooo!!!🙆‍♂️🙆‍♂️🙆‍♂️ Oga go loose weight ooo🤣🤣🤣')
+            
             elif conv_ <= 74:
                print('Your Weight is Nice. Don\'t add more ooo😂😂')
+               
         break
             
     elif ans.lower() == 'no':
